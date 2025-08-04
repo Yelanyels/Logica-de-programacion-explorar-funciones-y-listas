b54655086902a15170fe198cc3f8b529df09bb0c
Junnior Cueva <laiger_live_01@hotmail.com>
2025-08-04 16:08:05 -0500

    Actualización

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,11 @@
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+}
+
+function intentoDeUsuario() {
+  alert("Click desde el botón");
+}
+
+asignarTextoElemento("h1", "Juego del número secreto!");
+asignarTextoElemento("p", "Indica un número del 1 al 100");
