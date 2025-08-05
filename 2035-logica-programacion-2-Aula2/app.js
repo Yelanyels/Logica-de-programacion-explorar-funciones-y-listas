df91bc2b252d0442bb2344ad2272dcdd0b10a449
Junnior Cueva <laiger_live_01@hotmail.com>
2025-08-05 12:41:41 -0500

    Actualización

--- 2035-logica-programacion-2-Aula2/app.js
+++ 2035-logica-programacion-2-Aula2/app.js
@@ -1,5 +1,7 @@
 let numeroSecreto = generarNumeroSecreto();
-let intentos = 0;
+let intentos = 1;
+
+console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
@@ -11,15 +13,25 @@ function verificarIntento() {
   let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
 
   if (numeroDeUsuario === numeroSecreto) {
-    asignarTextoElemento("p", "Acertaste el número");
+    asignarTextoElemento(
+      "p",
+      `Acertaste el número en ${intentos} ${intentos === 1 ? "vez" : "veces"}`
+    ); document.getElementById("reiniciar").removeAttribute("disabled");
   } else {
+    //El usuario no acertó.
     if (numeroDeUsuario > numeroSecreto) {
       asignarTextoElemento("p", "El número secreto es menor");
     } else {
       asignarTextoElemento("p", "El número secreto es mayor");
     }
+    intentos++;
+    limpiarCaja();
   }
   return;
+}
+
+function limpiarCaja() {
+  document.querySelector("#valorUsuario").value = "";
 }
 
 function generarNumeroSecreto() {
@@ -28,3 +40,25 @@ function generarNumeroSecreto() {
 
 asignarTextoElemento("h1", "Juego del número secreto!");
 asignarTextoElemento("p", `Indica un número del 1 al 10`);
+
+/*Has sido contratado(a) para desarrollar una calculadora que calcula el promedio y así verificar la situación de aprobación de un estudiante en función de sus cuatro notas. El promedio requerido para aprobar es de mínimo 5. Tu tarea es implementar dos funciones en JavaScript:
+
+calcularPromedio(nota1, nota2, nota3, nota4) => Esta función recibe las cuatro notas del estudiante como parámetros y devuelve el promedio calculado con base en esas notas.
+verificarAprobacion(promedio) => Esta función recibe el promedio del estudiante como parámetro y devuelve "Aprobado" si el promedio es mayor o igual a 5, en caso contrario, devuelve "Reprobado".
+Utiliza estos valores para las calificaciones:
+*/
+/*
+let nota1 = 7;
+let nota2 = 6;
+let nota3 = 3;
+let nota4 = 5;
+
+function calcularPromedio(nota1, nota2, nota3, nota4) {
+  let promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+  return promedio;
+}
+
+function verificarAprobacion(promedio) {
+  return promedio >= 5 ? "Aprobado" : "Reprobado";
+}
+*/
